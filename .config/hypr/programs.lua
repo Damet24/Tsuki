3f9d5129192087f74409351142059f4867f78c00
Damet24 <damet.2401@gmail.com>
2026-05-26 22:42:31 -0500

    eww config updated and key bindings for hyperland

--- .config/hypr/programs.lua
+++ .config/hypr/programs.lua
@@ -3,6 +3,6 @@ return {
 	fileManager = "dolphin",
 	browser = "MOZ_ENABLE_WAYLAND=1 LIBVA_DRIVER_NAME=nouveau firefox",
 	editor = "nvim",
-	appLauncher = "rofi -show drun",
+	appLauncher = "rofi -show drun -show-icons -sidebar-mode -theme ~/.config/rofi/config.rasi -display-drun ' 󰍉 '",
 	spotify = "flatpak run com.spotify.Client",
 }
